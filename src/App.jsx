eab1fa0092b1df69152ed3d259e69d493f303bb0
Justyna <justynasadlak10@gmail.com>
2019-05-09 19:43:20 +0200

    Initial axios configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import FlashcardsList from './components/FlashcardsList';
 
 export default class App extends React.Component {
     render() {
         return (
             <div>
                 <h1>Flashcards</h1>
+                <FlashcardsList />
             </div>
         )
     }
---
+++ src/components/FlashcardsList.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+import axios from "axios";
+
+export default class FlashcardsList extends React.Component {
+  state = {
+    flashcards: []
+  };
+  handleFirstChange = event => {
+    this.setState({ firstText: event.target.value });
+  };
+  handleSecondChange = event => {
+    this.setState({ secondText: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const flashcard = {
+      firstText: this.state.firstText,
+      secondText: this.state.secondText
+    };
+console.log(flashcard);
+    axios.post(`http://localhost:4000/api/flashcards`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: {
+      JSON.stringify({ flashcard })
+      }
+    })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      });
+  };
+  componentDidMount() {
+    axios.get(`http://localhost:4000/api/flashcards`).then(res => {
+      const flashcards = res.data;
+      this.setState({ flashcards });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.flashcards.map(flashcard => (
+            <li key="{flashcard.id}">{flashcard.firstText}</li>
+          ))}
+        </ul>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            FIrst:
+            <input
+              type="text"
+              name="firstText"
+              onChange={this.handleFirstChange.bind(this)}
+            />
+          </label>
+          <label>
+            Second:
+            <input
+              type="text"
+              name="secondText"
+              onChange={this.handleSecondChange.bind(this)}
+            />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    );
+  }
+}
