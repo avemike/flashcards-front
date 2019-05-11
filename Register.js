479f8d10b45bae9528c2e2aa272c4a4fc24e993b
Rafał Płoszka <47782567+RafalPloszka@users.noreply.github.com>
2019-05-12 01:16:26 +0200

    Add files via upload

---
+++ Register.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
+import axios from 'axios';
+import Login from './Login';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#3cc26f',
+    },
+  }
+});
+
+class Register extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      email:'',
+      password:''
+    }
+  }
+  componentWillReceiveProps(nextProps){
+    console.log("nextProps",nextProps);
+  }
+  handleClick(event){
+    var apiBaseUrl = "http://localhost:4000/api/";
+    // console.log("values in register handler");
+    var self = this;
+    //To be done:check for empty values before hitting submit
+    if(this.state.email.length>0 && this.state.password.length>0){
+      var payload={
+      "userid":this.state.email,
+      "password":this.state.password
+      }
+      axios.post(apiBaseUrl+'/register', payload)
+     .then(function (response) {
+       console.log(response);
+       if(response.data.code === 200){
+         console.log("registration successfull");
+         var loginscreen=[];
+         loginscreen.push(<Login parentContext={this} appContext={self.props.appContext}/>);
+         var loginmessage = "Jeszcze niezarejestrowany? Załóż konto!";
+         self.props.parentContext.setState({loginscreen:loginscreen,
+         loginmessage:loginmessage,
+         buttonLabel:"Zarejestruj",
+         isLogin:true
+          });
+       }
+       else{
+         console.log("some error ocurred",response.data.code);
+       }
+     })
+     .catch(function (error) {
+       console.log(error);
+     });
+    }
+    else{
+      alert("Uzupełnij wszystkie pola.");
+    }
+
+  }
+  render() {
+    // console.log("props",this.props);
+    return (
+      <div>
+        <MuiThemeProvider theme={theme}>
+          <div>
+          <AppBar style={greenStyle}
+             title="Rejestracja"
+           />
+           <TextField 
+             hintText="Podaj swój email"
+             floatingLabelText="Email"
+             onChange = {(event,newValue) => this.setState({email:newValue})}
+             />
+           <br/>
+           <TextField
+             type = "password"
+             hintText="Podaj hasło"
+             floatingLabelText="Hasło"
+             onChange = {(event,newValue) => this.setState({password:newValue})}
+             />
+           <br/>
+           <RaisedButton label="Zarejestruj" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+          </div>
+         </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+
+const style = {
+  margin: 15,
+};
+
+const greenStyle = {
+  background: '#3cc26f'
+}
+
+
+export default Register;
---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+document.body.style = 'background:  rgb(221, 250, 221);';
