fc2f12b79d477150d80ab1fdbcbc19a17c6d8534
Marta Kołodziej <marta.koodziej@gmail.com>
2019-05-11 15:57:40 +0200

    Answer checking - style

--- src/components/AnswerChecker.js
+++ src/components/AnswerChecker.js
@@ -1,11 +1,15 @@
+import "../style/AnswerChecker.css";
 import React from 'react';
 
 export default class AnswerChecker extends React.Component {
+    properValueId = "properValue";
+
     state = {
         userAnswer: ""
     };
 
     checkUserAnswer = (event) => {
+        document.getElementById(this.properValueId).innerHTML = this.props.flashcard.answer;
         this.isAnswerCorrect() ? this.setAnswerCorrect() : this.setAnswerIncorrect();
 
         console.log("Correct answers " + this.props.flashcard.correctAnswer);
@@ -14,21 +18,27 @@ export default class AnswerChecker extends React.Component {
 
 
     isAnswerCorrect = () => {
-        return this.state.userAnswer === this.props.flashcard.answer;
+        return this.state.userAnswer.toLowerCase() === this.props.flashcard.answer.toLowerCase();
     };
 
     setAnswerCorrect = () => {
-        this.props.flashcard.correctAnswer += 1; 
+        this.props.flashcard.correctAnswer += 1;
+        this.setTextFrameStyle("properAnswer");
     }
 
     setAnswerIncorrect = () => {
         this.props.flashcard.badAnswers += 1;
+        this.setTextFrameStyle("wrongAnswer");
+    }
+
+    setTextFrameStyle = (className) => {
+        document.getElementById("textButtonContainer").getElementsByTagName("textarea")[0].className = className;
     }
 
     render() {
         return (
-            <div className="textButtonContainer">
-                <div id="properValue"></div>
+            <div id="textButtonContainer">
+                <div id={this.properValueId}></div>
                 <textarea rows="2" cols="44"
                     onChange={e => this.setState({userAnswer:e.target.value})}></textarea>
                 <button onClick={this.checkUserAnswer}>{this.props.name}</button>
