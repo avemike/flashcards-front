d038a34fd80d7a0bf52fc76e77bd5018fb36b7a1
Kuba Fila <kontakt@kubafila.pl>
2019-05-13 17:24:25 +0200

    Created first version of Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div>
-                <h1>Flashcards</h1>
+            
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.jsx';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Feature from './Feature'
+import ButtonLanding from './Button'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () =>{
+	return (
+		<div>
+			<ButtonLanding
+				text="Załóż konto!"
+				onClick=""
+			 />
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+		</div>
+	);
+}
+
+const Features = () =>{
+	return (
+		<div className="features-body">
+			<Feature
+				title="Szybkie"
+				icon="fas fa-space-shuttle"
+				text="Praca z naszymi fiszkami jest błyskawiczna! Nie ważne czy masz przerwę, chwilę wolnego, jedziesz autobusem czy uczysz się na egzamin. Z nami szybko i skutecznie osiągniesz swoje cele!"
+			/>
+			<Feature
+				title="Lekkie"
+				icon="fas fa-paper-plane"
+				text="Fiszki działają tylko na naszych serwerach. Nie ma potrzeby ściągania niepotrzebnych apek, programów czy wtyczek. Wszytko dzieję się w twojej przeglądarce!"
+			/>
+			<Feature
+				title="Przyjemne"
+				icon="fas fa-hand-peace"
+				text="Nauka z naszymi fiszkami to czysta przyjemność! Z radością zauważysz postępy, i docenisz możliwości jakie otworzą się przed tobą dzięki nauce nowego języka"
+			/>
+
+	</div>
+
+
+		
+	);
+}
+
+ReactDOM.render(<App />,document.getElementById('root'))
+ReactDOM.render(<Features />, document.getElementById('features'))
+
