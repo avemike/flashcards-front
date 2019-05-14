b9863b4fa398f6671ed5307c9ff84bf63c35d017
avemike <fajny.michal.gajek@gmail.com>
2019-05-14 18:44:08 +0200

    redirected to register after trying enter test, edit or mode page

--- src/chooseModePage/App.jsx
+++ src/chooseModePage/App.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import Choice from './Choice'
+import axios from 'axios';
+import history from '../history';
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = { authorized: false }
+
+        axios.get('http://localhost:4000/api/register/me', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-auth-token': localStorage.getItem("userKey")
+            }
+        })
+        .then(res => {
+            // User is logged in
+            this.setState({authorized: true})
+
+        })
+        .catch(err => {
+            // Redirect to home
+            history.push('/loginRegister')
+        })
+    }
     render() {
-        return (
-          <Choice />
-        )
+        return this.state.authorized ? <Choice /> : <div></div>
     }
 }
--- src/editFlashcardsPage/App.jsx
+++ src/editFlashcardsPage/App.jsx
@@ -3,11 +3,32 @@ import './App.css';
 import './style.css'
 import Header from './Header/Header';
 import Content from './Content/Content'
+import axios from 'axios';
+import history from '../history';
 
 export default class App extends Component {
-
+    constructor() {
+        super();
+        this.state = { authorized: false }
+        axios.get('http://localhost:4000/api/register/me', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'x-auth-token': localStorage.getItem("userKey")
+            }
+        })
+        .then(res => {
+            // User is logged in
+            this.setState({authorized: true})
+            
+        })
+        .catch(err => {
+            // Redirect to home
+            history.push('/loginRegister')
+        })
+    }
     render() {
-        return (
+        if(this.state.authorized) return (
             <div className='screen'>
                 <div className='container'>
                     <Header />
@@ -15,6 +36,7 @@ export default class App extends Component {
                 </div>
             </div>
         );
+        else return <div></div>    
     }
 }
 
