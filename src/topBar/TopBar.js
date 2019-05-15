bff731b765aae0ec6cf31348e5845862cb0c138d
avemike <fajny.michal.gajek@gmail.com>
2019-05-15 18:55:47 +0200

    testFlashcards page height fixed

--- src/topBar/TopBar.js
+++ src/topBar/TopBar.js
@@ -3,11 +3,11 @@ import React from 'react';
 
 
 const TopBarStyle = {
-	height:"60px",
+	height:"60px",  // testFlashards page height is calc(100vh - 60px)
 	lineHeight:'60px',
 	width:"100%",
 	background:"#04291D",
-	margin:" 0 0 30px 0 ",
+	margin:" 0 0 0 0 ",
 	padding:"0",
 	display:"flex",
 	justifyContent: "space-between",
